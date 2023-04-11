559e2c531436fb4bf060900e9639494cc3073f94
heysahilsingh <119090408+heysahilsingh@users.noreply.github.com>
2023-04-11 11:32:16 +0530

    Update Remove Unwanted CSS_Assets From WP.php

--- Remove Unwanted CSS_Assets From WP.php
+++ Remove Unwanted CSS_Assets From WP.php
@@ -23,6 +23,9 @@ add_action( 'wp_enqueue_scripts', function(){
 remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
 remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
 
+// Remove Woocommerce Default CSSs
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
 //Remove CSS assets from Gutenberg Blocks & WooCommerce Blocks
 add_action( 'wp_enqueue_scripts', function() {
     wp_dequeue_style( 'wp-block-library' );
@@ -30,13 +33,31 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_dequeue_style( 'wc-blocks-style' );
 }, 100 );
 
-//Remove Elementor Icon's CSS
-add_action( 'wp_enqueue_scripts', 'disable_eicons', 11 );
-function disable_eicons() {
-	wp_dequeue_style( 'elementor-icons' );
-	wp_deregister_style( 'elementor-icons' );}
+// Remove id='classic-theme-styles-css' From Header
+add_action( 'wp_enqueue_scripts', 'mywptheme_child_deregister_styles', 20 );
+function mywptheme_child_deregister_styles() {
+    wp_dequeue_style( 'classic-theme-styles' );
+}
 
-//Elementor - disable Google Fonts
-add_filter( 'elementor/frontend/print_google_fonts', 'return_false' );
-add_filter( 'elementor/fonts/additional_fonts', 'return_false' );
+//Remove id='dashicons-css'
+function disable_dashicons() {
+   wp_deregister_style('dashicons');
+}
+add_action('wp_enqueue_scripts', 'disable_dashicons');
+
+//Elementor - disable font awesome
+add_action( 'elementor/frontend/after_register_styles',function() {
+    foreach( [ 'solid', 'regular', 'brands' ] as $style ) {
+        wp_deregister_style( 'elementor-icons-fa-' . $style );
+    }
+}, 20 );
+
+//Remove Elementor Icon's + Font Awesome 2 CSS
+function disable_elementor_font_awesome() {
+    wp_deregister_style('elementor-icons');
+    wp_deregister_style('font-awesome');
+    wp_dequeue_style('elementor-icons');
+    wp_dequeue_style('font-awesome');
+}
+add_action('wp_enqueue_scripts', 'disable_elementor_font_awesome', 9999);
 /*============== REMOVE UNWANTED WP ASSETS END ==============*/
